ad6badd00b219be657845e3346ef6f79b7eedcc0
Ging Gonzalez <ginggonzalez3@gmail.com>
2017-12-24 01:53:28 -0800

    style: Styles the message paragraph

--- src/components/MessagePage/MessagePage.js
+++ src/components/MessagePage/MessagePage.js
@@ -3,6 +3,7 @@ import { PropTypes } from 'prop-types'
 import { connect } from 'react-redux'
 
 import GoogleLoginButton from '../GoogleLoginButton/GoogleLoginButton'
+import AnonymousLogin from '../AnonymousLogin/AnonymousLogin'
 
 class MessagePage extends Component {
     state = {}
@@ -14,10 +15,18 @@ class MessagePage extends Component {
             <div style={styles.container}>
                 <div style={styles.header}>
                     <GoogleLoginButton style={styles.loginButton} />
+                    <AnonymousLogin style={styles.loginButton} />
                 </div>
-                <div style={styles.message}>
-                    {message}
+                <div style={styles.messageContainer}>
+                    <div style={styles.clear}>
+                    </div>
+                    <p align="justify" style={styles.message}>
+                        {message}
+                    </p>
+                    <div style={styles.clear}>
+                    </div>
                 </div>
+
             </div>
         )
     }
@@ -35,8 +44,21 @@ const styles = {
     loginButton: {
 
     },
+    messageContainer: {
+        display: 'flex',
+        flex: '1',
+        flexWrap: 'wrap',
+    },
     message: {
-        
+        flex: '3',
+        fontSize: '1em',
+        padding: '1em',
+        margin: '0px',
+        textIndent: '2em',
+        lineHeight: '1.45',
+    },
+    clear: {
+        flex: '1',
     }
 }
 
